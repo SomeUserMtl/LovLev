15371585c70ebdd51be5c0c89bb6cd7c789c7ccf
HersheyB <hershybm@gmail.com>
2022-11-24 15:25:21 -0500

    validation for partner attributes

--- src/main/java/com/project/lovlev/models/Partner.java
+++ src/main/java/com/project/lovlev/models/Partner.java
@@ -3,35 +3,46 @@ package com.project.lovlev.models;
 import com.project.lovlev.enums.Colors;
 import com.project.lovlev.enums.Sex;
 import lombok.Data;
-
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Data
 @Entity(name = "partner")
 public class Partner{
 
+    @Null(message = "id must be null")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Pattern(regexp = "^[a-zA-Z\s]{0,20}$", message = "First name can only contain letters or spaces")
     @Column(name = "first_name")
     private String firstName;
 
+    @Pattern(regexp = "^[a-zA-Z\s]{0,20}$", message = "Middle name can only contain letters or spaces")
     @Column(name = "middle_name")
     private String middleName;
 
+    @Pattern(regexp = "^[a-zA-Z\s]{0,20}$", message = "last name can only contain letters or spaces")
     @Column(name = "last_name")
     private String lastName;
 
+    @Pattern(regexp = "^[a-zA-Z0-9]{0,20}$", message = "nickname can only contain letters or numbers")
     @Column(name = "nickname")
     private String nickname;
 
+    @Min(value = 0, message = "age must be positive")
+    @Max(value = 120, message = "age must be less than 120")
     @Column(name = "age")
     private int age;
 
+    @Min(value = 0, message = "feet height must be greater than 0")
+    @Max(value = 8, message = "feet height must be less than 8")
     @Column(name = "height_feet")
     private int heightFeet;
 
+    @Min(value = 0, message = "inches height must be greater than 0")
+    @Max(value = 11, message = "inches height must be less than 11")
     @Column(name = "height_inches")
     private int heightInches;
 
@@ -44,6 +55,7 @@ public class Partner{
     @Enumerated(EnumType.ORDINAL)
     private Colors eyeColor;
 
+    @Column(name = "sex")
     @Enumerated(EnumType.ORDINAL)
     private Sex sex;
 
