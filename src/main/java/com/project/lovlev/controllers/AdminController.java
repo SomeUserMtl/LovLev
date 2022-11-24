bb80cde755b385d8d3c6f41ca2cf8776d1baa79c
HersheyB <hershybm@gmail.com>
2022-11-24 17:32:55 -0500

    other

---
+++ src/main/java/com/project/lovlev/controllers/AdminController.java
@@ -0,0 +1,10 @@
+package com.project.lovlev.controllers;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.RestController;
+
+@Data
+@RestController
+@
+public class AdminController {
+}
---
+++ src/main/java/com/project/lovlev/models/Roles.java
@@ -0,0 +1,8 @@
+package com.project.lovlev.models;
+
+import lombok.Data;
+
+@Data
+public class Roles {
+    //need many to many roles table and ensure user can't assign themselves roles
+}
