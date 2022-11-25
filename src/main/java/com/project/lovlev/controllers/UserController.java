2961bb19202943345c7af1b629169475fa3ea92e
HersheyB <hershybm@gmail.com>
2022-11-24 21:50:41 -0500

    allow non authenticated acces to /register

--- src/main/java/com/project/lovlev/controllers/UserController.java
+++ src/main/java/com/project/lovlev/controllers/UserController.java
@@ -8,12 +8,13 @@ import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.PermitAll;
 import javax.validation.Valid;
 
 @Data
@@ -33,13 +34,12 @@ public class UserController {
     }
 
     // Create user
-    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
-    @Secured("ROLE_ADMIN")
-    @PostMapping(path = "user",
+    @PermitAll
+    @PostMapping(path = "register",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> save(@RequestBody @Valid User newUser,
-                                     Authentication authentication) throws Exception {
+    public ResponseEntity<User> register(@RequestBody @Valid User newUser,
+                                         Authentication authentication) throws Exception {
 
         SecurityUser userPrincipal = (SecurityUser) authentication.getPrincipal();
 
@@ -59,7 +59,34 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
-    // Delete user
+
+//    @PreAuthorize("anonymous")
+//    @PostMapping(path = "login",
+//            consumes = MediaType.APPLICATION_JSON_VALUE,
+//            produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<User> login(@RequestBody @Valid User newUser,
+//                                         Authentication authentication) throws Exception {
+//
+//        SecurityUser userPrincipal = (SecurityUser) authentication.getPrincipal();
+//
+//        // ugly solution to disallow non-admins from adding roles
+//        if (newUser.getRoles() != null && userPrincipal
+//                .getAuthorities()
+//                .stream()
+//                .map(Object::toString)
+//                .noneMatch(s -> s.equals("ROLE_ADMIN"))) {
+//            throw new IllegalArgumentException("You are not authorized to add roles");
+//        }
+//
+//        customValidators.validatePassword(newUser.getPassword());
+//
+//        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+//        User user = userRepository.save(newUser);
+//        return new ResponseEntity<>(user, HttpStatus.CREATED);
+//}
+
+
+        // Delete user
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @DeleteMapping("user")
     public ResponseEntity<User> delete(@RequestParam Long id) {
