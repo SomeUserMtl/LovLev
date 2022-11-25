a2fe23367f07e476d6a9c251619cd56fe07a399c
HersheyB <hershybm@gmail.com>
2022-11-25 11:03:52 -0500

    updated to use global authenticated object and handled null pointer for no auth users when checking for roles

--- src/main/java/com/project/lovlev/controllers/UserController.java
+++ src/main/java/com/project/lovlev/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.project.lovlev.controllers;
 
+import com.project.lovlev.configs.IAuthenticationFacade;
 import com.project.lovlev.models.SecurityUser;
 import com.project.lovlev.models.User;
 import com.project.lovlev.repositories.UserRepository;
@@ -8,13 +9,9 @@ import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.security.PermitAll;
 import javax.validation.Valid;
 
 @Data
@@ -24,43 +21,46 @@ public class UserController {
     UserRepository userRepository;
     PasswordEncoder passwordEncoder;
     CustomValidators customValidators;
+    IAuthenticationFacade authentication;
 
     public UserController(UserRepository userRepository,
                           PasswordEncoder passwordEncoder,
-                          CustomValidators customValidators) {
+                          CustomValidators customValidators,
+                          IAuthenticationFacade authentication) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.customValidators = customValidators;
+        this.authentication = authentication;
     }
 
     // Create user
-    @PermitAll
+//    @PermitAll
     @PostMapping(path = "register",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> register(@RequestBody @Valid User newUser,
-                                         Authentication authentication) throws Exception {
+    public ResponseEntity<User> register(@RequestBody @Valid User newUser) throws Exception {
 
-        SecurityUser userPrincipal = (SecurityUser) authentication.getPrincipal();
-
-        // ugly solution to disallow non-admins from adding roles
-        if (newUser.getRoles() != null && userPrincipal
-                .getAuthorities()
-                .stream()
-                .map(Object::toString)
-                .noneMatch(s -> s.equals("ROLE_ADMIN"))) {
-            throw new IllegalArgumentException("You are not authorized to add roles");
+        // Ensure only users with ROLE_ADMIN can create users with ROLE_ADMIN, rest it's ROLE_USER
+        if (authentication.getAuthentication().getPrincipal() == "anonymousUser")
+            newUser.setRoles("ROLE_USER");
+        else {
+            SecurityUser userPrincipal = ((SecurityUser) authentication
+                    .getAuthentication()
+                    .getPrincipal());
+            if (!userPrincipal.hasRole("ROLE_ADMIN")) {
+                newUser.setRoles("ROLE_USER");
+            }
         }
 
         customValidators.validatePassword(newUser.getPassword());
-
         newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+
         User user = userRepository.save(newUser);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
 
-//    @PreAuthorize("anonymous")
+//    @PreAuthorize("'ROLE_ANONYMOUS' != authentication.principal.authorities")
 //    @PostMapping(path = "login",
 //            consumes = MediaType.APPLICATION_JSON_VALUE,
 //            produces = MediaType.APPLICATION_JSON_VALUE)
@@ -87,7 +87,7 @@ public class UserController {
 
 
         // Delete user
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @DeleteMapping("user")
     public ResponseEntity<User> delete(@RequestParam Long id) {
         userRepository.deleteById(id);
