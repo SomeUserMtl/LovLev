407537fbe7e3e1f844a26ce53948be1bbac9c17b
HersheyB <hershybm@gmail.com>
2022-11-25 11:14:53 -0500

    updated to remove uneeded elements

--- src/main/java/com/project/lovlev/configs/SecurityConfig.java
+++ src/main/java/com/project/lovlev/configs/SecurityConfig.java
@@ -3,9 +3,7 @@ package com.project.lovlev.configs;
 import com.project.lovlev.services.JpaUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,8 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Configuration
@@ -24,23 +20,10 @@ import java.util.Optional;
 public class SecurityConfig {
 
     private final JpaUserDetailService jpaUserDetailService;
-//
+
     public SecurityConfig(JpaUserDetailService jpaUserDetailService) {
         this.jpaUserDetailService = jpaUserDetailService;
     }
-
-    //Hardcoded for development purposes
-
-//    @Bean
-//    public InMemoryUserDetailsManager user(){
-//        System.out.println(passwordEncoder().encode("user"));
-//        return new InMemoryUserDetailsManager(
-//                User.withUsername("user")
-//                        .password(passwordEncoder().encode("user"))
-//                        .authorities("read")
-//                        .build()
-//        );
-//    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -48,8 +31,8 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(authorize ->
                         authorize
-                                .antMatchers("/admin/**").hasRole("ADMIN")
-                                .antMatchers("/user/**").hasRole("USER")
+                                .antMatchers("/admin/**").hasRole("ROLE_ADMIN")
+                                .antMatchers("/user/**").hasAnyRole("'ROLE_USER', 'ROLE_ADMIN'")
                                 .antMatchers("/register/**").permitAll()
                                 .anyRequest().authenticated()
                 )
@@ -59,15 +42,6 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .build();
-//        return http
-//                .csrf().disable()
-//                .authorizeRequests((authorize) -> authorize
-////                        .antMatchers(HttpMethod.GET,"/admin/users/**").authenticated()
-//                        .anyRequest().authenticated())
-//                .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults())
-////                .userDetailsService(jpaUserDetailService)
-//                .build();
     }
 
     @Bean
@@ -75,12 +49,5 @@ public class SecurityConfig {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         System.out.println("passwordEncoder: " + bCryptPasswordEncoder);
         return new BCryptPasswordEncoder();
-    }
-
-    // Get principal
-    @Bean
-    public Optional<Authentication> getAuthentication() {
-        System.out.println("getAuthentication: " + SecurityContextHolder.getContext().getAuthentication());
-        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication());
     }
 }
